82d6d2899cb2458e3798b67542bc86341ade8d4c
Eduardo <eduardoleao052@gmail.com>
2024-11-02 14:32:01 -0300

    adicionar segunda versao com forms

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.posts_list, name="list"),
+    path('post-new/', views.post_new, name="post-new"),
     path('<slug:slug>', views.post_page, name="page")
 ]
